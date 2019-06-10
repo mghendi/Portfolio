6f8a715bd4b5749a08b385191e9d3d48eb842df3
Samuel Mwamburi <sam.mwamburi@gmail.com>
2019-06-10 11:38:52 +0300

    base.html

--- django_blog/my-venv/Scripts/src/django_blog/view.py
+++ django_blog/my-venv/Scripts/src/django_blog/view.py
@@ -2,13 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home.html")
+    home_title = "Hi, I'm Sammy."
+    #doc = "<h1>{title}</h1>".format(title=title)
+    #django_rendered_doc = "<h1>{{title}}</h1>".format(title=title)
+    return render(request, "home.html", {"title": home_title})
 
 def work(request):
-    return render(request, "work.html")
+    return render(request, "work.html", {"title": "Things I've Learnt"})
 
 def blog(request):
-    return render(request, "blog.html")
+    return render(request, "blog.html", {"title": "Tips, Tricks & Tutorials"})
 
 def resume(request):
-    return render(request, "resume.html")
+    return render(request, "resume.html", {"title": "Resume"})
